09e7ad6e2712c8dcf98da95da4a49459be6febd0
Reka Kulinyi <reekaa85@gmail.com>
2018-04-03 17:48:09 +0200

    error handling added

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,35 @@
 window.onload = function() {
-  const sf = document.getElementById("search-field")
-  sf.value="";
+  const searchField = document.getElementById("search-field")
+  searchField.value="";
   const results = document.getElementById("result-list");
+  const lang = document.getElementById("language-selector")
 
-  sf.addEventListener("keyup", function(event) {
+  searchField.addEventListener("keyup", function(event) {
     event.preventDefault();
     if (event.keyCode === 13) {
           results.innerHTML = "";
-          fetch(`https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=${sf.value}&limit=5`)
-          .then(response => response.json())
+          fetch(`https://en.wikipedia.org/w/api.php?&origin=*&action=opensearch&search=${searchField.value}&limit=5`)
+          .then(response => {
+            if(!response.ok){
+              throw new Error(response.statusText);
+            }
+            return response.json();
+          })
           .then((data) => {
-            data[1].forEach(function(elem){
-              const elemIndex = data[1].indexOf(elem)
-              const item = `<li><a href="${data[3][elemIndex]}" target="_blank">${elem}</a></li>`;
-              results.insertAdjacentHTML("beforeend", item);
+            if (data[1].length === 0){
+              results.insertAdjacentHTML("afterbegin", `The keyword \"${searchField.value}\" was not found on wikipedia`)
+            } else {
+              console.log(data)
+              data[1].forEach(function(value, index){
+                const item = `<li><a href="${data[3][index]}" target="_blank">${value}</a>
+                              <p>${data[2][index].substring(0,80)}..</p>
+                              </li>`;
+                results.insertAdjacentHTML("beforeend", item);
+              });
+            }
+          }).catch(function(error){
+              results.insertAdjacentHTML("afterbegin", "There was an error, please try again")
             });
-          });
     };
   });
 };
